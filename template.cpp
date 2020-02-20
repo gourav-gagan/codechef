b7c327bc7f0ef9cdef030e75e936b6c2317a7ceb
gourav-gagan <54671216+gourav-gagan@users.noreply.github.com>
2020-02-20 11:20:27 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,9 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void solution()
+{
+    //CODE HERE
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    unsigned short int t;
+    cin>>t;
+    while(t--)
+    {
+        solution();
+        cout<<'\n';
+    }
     return 0;
 }
